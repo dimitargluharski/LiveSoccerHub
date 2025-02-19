3cdcdd14263d1ea21260fbf4a985c147569352f5
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-02-19 23:35:45 +0200

    enhancement search logic

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -7,45 +7,45 @@ import { HomePageInputSearchField } from "../../components/HomePageInputSearchFi
 export const HomePage = () => {
   const [games, setGames] = useState([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [allGames, setAllGames] = useState([]);
 
   useEffect(() => {
-    if (!searchTerm) {
-      footballService.getLiveGames().then(response => setGames(response)).catch((err) => console.log(err));
-    }
-  }, [searchTerm]);
+    footballService.getLiveGames()
+      .then(response => {
+        setGames(response);
+        setAllGames(response);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchValue = event.target.value.toLowerCase();
-
     setSearchTerm(searchValue);
 
-    const filteredArray = games.filter((game) =>
-      // @ts-ignore
-      game.teams.home.name.toLowerCase().includes(searchValue.toLowerCase()) ||
-      // @ts-ignore
-      game.teams.away.name.toLowerCase().includes(searchValue.toLowerCase())
-    );
-
-    setGames(filteredArray);
+    if (!searchValue) {
+      setGames(allGames);
+    } else {
+      const filteredArray = allGames.filter((game) =>
+        // @ts-ignore
+        game.teams.home.name.toLowerCase().includes(searchValue) ||
+        // @ts-ignore
+        game.teams.away.name.toLowerCase().includes(searchValue)
+      );
+      setGames(filteredArray);
+    }
   };
-
-  console.log(games);
-
-  if (games.length === 0) {
-    return (
-      <div className="text-3xl text-center text-slate-400">
-        Sorry, but there is no data to display.
-      </div>
-    )
-  }
 
   return (
     <div className="flex flex-col gap-2">
       <HomePageInputSearchField handleInputChange={handleInputChange} searchTerm={searchTerm} />
 
-      {games.map((game, index) => (
-        <CardTemplate key={index} data={game} />
-      ))}
+      {games.length ? (
+        games.map((game, index) => (
+          <CardTemplate key={index} data={game} />
+        ))
+      ) : (
+        <div className="text-center text-gray-500 mt-4">No matches found for your search.</div>
+      )}
     </div>
-  )
-}
+  );
+};
