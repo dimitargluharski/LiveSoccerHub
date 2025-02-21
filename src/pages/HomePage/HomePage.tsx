6ed5b277c5bbd7a97c773f519a6b5d2b1be09803
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-02-22 01:10:44 +0200

    add favorite section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import * as footballService from '../../services/getLiveGames';
 import { CardTemplate } from "../../components/CardTemplate/CardTemplate";
 import { HomePageInputSearchField } from "../../components/HomePageInputSearchField/HomePageInputSearchField";
+import { FavoriteGame } from "../../components/FavoriteGame/FavoriteGame";
 
 export const HomePage = () => {
   const [games, setGames] = useState([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [allGames, setAllGames] = useState([]);
+  const [favorite, setFavorite] = useState([]);
 
   useEffect(() => {
     footballService.getLiveGames()
@@ -36,13 +38,25 @@ export const HomePage = () => {
     setGames(filteredArray);
   };
 
+  const handleAddGameToFavorite = (game: unknown) => {
+    setFavorite((oldValue) => [...oldValue, game])
+
+    localStorage.setItem('favoriteGames', JSON.stringify(favorite));
+  };
+
+  useEffect(() => {
+    console.log('added new item!');
+  }, [favorite.length])
+
   return (
     <div className="flex flex-col gap-2">
       <HomePageInputSearchField handleInputChange={handleInputChange} searchTerm={searchTerm} />
 
+      {favorite.length > 0 && <FavoriteGame favorite={favorite} />}
+
       {games.length ? (
         games.map((game, index) => (
-          <CardTemplate key={index} data={game} />
+          <CardTemplate key={index} data={game} handleAddGameToFavorite={() => handleAddGameToFavorite(game)} />
         ))
       ) : (
         <div className="text-center text-gray-500 mt-4">No matches found for your search.</div>
