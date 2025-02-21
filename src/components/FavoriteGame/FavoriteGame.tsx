9bae78adde92616e822779dbc21e8c1e405bf7b7
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-02-22 01:10:30 +0200

    commented out the jsx

---
+++ src/components/FavoriteGame/FavoriteGame.tsx
@@ -0,0 +1,19 @@
+
+
+export const FavoriteGame = () => {
+  return (
+    // <section>
+    //   {favorite.length} saved {favorite.length === 1 ? 'game' : 'games'}
+    //   {favorite.length ? (favorite.map((game, index) => (
+    //     <div className="flex w-full gap-2">
+    //       <div key={index}>
+    //         {game.fixture.status.elapsed === 90 ? (<div>{game.fixture.status.elapsed}+{game.fixture.status.extra}'</div>) : game.fixture.status.elapsed}
+    //       </div>
+    //       <div className="flex justify-center">
+    //         {game.teams.home.name} {game.goals.home} - {game.goals.away} {game.teams.away.name}
+    //       </div>
+    //     </div>
+    //   ))) : null}
+    // </section>
+  )
+}
