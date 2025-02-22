a347d8b5742b32d530b03287f64c12d1abe04981
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-02-22 13:10:26 +0200

    create favorite game component

--- src/components/FavoriteGame/FavoriteGame.tsx
+++ src/components/FavoriteGame/FavoriteGame.tsx
@@ -1,22 +1,82 @@
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
+import { Data } from "../../pages/HomePage/HomePage";
+import { AwayTeamCardTemplate } from "../AwayTeamCardTemplate/AwayTeamCardTemplate";
+import { HomeTeamCardTemplate } from "../HomeTeamCardTemplate/HomeTeamCardTemplate";
+import { TimeCardTemplate } from "../TimeCardTemplate/TimeCardTemplate";
 
-export const FavoriteGame = () => {
+type FavoriteGameTypes = {
+  data: {
+    fixture: {
+      id: number;
+      status: {
+        elapsed: number;
+        extra: number;
+        long: string;
+        short: string;
+      }
+    },
+    teams: {
+      home: {
+        id: number;
+        name: string;
+        winner: boolean;
+        logo: string;
+      },
+      away: {
+        id: number;
+        name: string;
+        winner: boolean;
+        logo: string;
+      }
+    },
+    score: {
+      halftime: {
+        home: number;
+        away: number;
+      },
+      fulltime: {
+        home: number;
+        away: number;
+      },
+      penalty: {
+        home: number;
+        away: number;
+      },
+      extratime: {
+        home: number;
+        away: number;
+      }
+    },
+    league: {
+      name: string;
+      flag: string;
+      country: string;
+      season: number;
+    },
+    goals: {
+      home: number;
+      away: number;
+    },
+  },
+  handleRemoveFromFavorite: (game: Data) => void;
+};
+
+export const FavoriteGame = ({ data, handleRemoveFromFavorite }: FavoriteGameTypes) => {
   return (
-    <div>
-      ddd
+    <div className="flex items-center gap-2 rounded-md p-2 hover:shadow-md shadow-sm">
+      <div>
+        <TimeCardTemplate time={data.fixture.status} />
+      </div>
+
+      <div className="w-full">
+        <HomeTeamCardTemplate teamName={data.teams.home.name} id={data.teams.home.id} goals={data.score} totalGoals={data.goals} winner={data.teams.home.winner} />
+        <AwayTeamCardTemplate teamName={data.teams.away.name} id={data.teams.away.id} goals={data.score} totalGoals={data.goals} winner={data.teams.away.winner} />
+      </div>
+
+      <div onClick={() => handleRemoveFromFavorite(data)}>
+        <IoMdCloseCircleOutline className="w-5 h-5 cursor-pointer text-red-500" />
+      </div>
     </div>
-    // <section>
-    //   {favorite.length} saved {favorite.length === 1 ? 'game' : 'games'}
-    //   {favorite.length ? (favorite.map((game, index) => (
-    //     <div className="flex w-full gap-2">
-    //       <div key={index}>
-    //         {game.fixture.status.elapsed === 90 ? (<div>{game.fixture.status.elapsed}+{game.fixture.status.extra}'</div>) : game.fixture.status.elapsed}
-    //       </div>
-    //       <div className="flex justify-center">
-    //         {game.teams.home.name} {game.goals.home} - {game.goals.away} {game.teams.away.name}
-    //       </div>
-    //     </div>
-    //   ))) : null}
-    // </section>
   )
 }
