73b1825e6b6113bec7ab510ed51723edabc9e60f
Dimitar Gluharski <dimitargluharski@gmail.com>
2025-04-24 20:00:39 +0300

    change styles

--- src/components/GameEvents/GameEvents.tsx
+++ src/components/GameEvents/GameEvents.tsx
@@ -27,7 +27,7 @@ interface Event {
 export const GameEvents = ({ events }: Event) => {
   return (
     <div className="flex justify-end text-slate-500">
-      {events.length ? (<div className={`flex items-center w-full p-2 ${events.length >= 10 ? 'dark:bg-red-500/60 bg-red-500/65 text-white rounded-md' : ''}`}>
+      {events.length ? (<div className={`flex items-center w-full p-2 ${events.length >= 10 ? 'dark:text-red-500/60 text-red-500/65  rounded-md' : ''}`}>
         {getEventCountString(events.length)}
       </div>) : 'N/A'}
     </div>
